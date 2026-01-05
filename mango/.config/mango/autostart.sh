8e1247b3d43906056d03d06b1b85dd90581cd13f
Mike Carter <carterjmike@gmail.com>
2026-01-05 12:22:30 -0500

    fix wm paths

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -7,3 +7,7 @@ dbus-update-activation-environment --all 2>&1 > /dev/null &
 waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css 2>&1 > /dev/null & 
 
 nm-applet --indicator 2>&1 > /dev/null &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 2>&1 > /dev/null &
+
+gnome-keyring-daemon --start --components=secret 2>&1 > /dev/null &
